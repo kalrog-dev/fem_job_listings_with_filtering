05160067db2540d0cbb27e418c2b3dbb1af8593d
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-11-05 13:56:25 +0100

    First commit

---
+++ src/data.jsx
@@ -0,0 +1,163 @@
+data = [
+  {
+    company: "Photosnap",
+    jobTitle: "Senior Frontend Developer",
+    date: "1d ago",
+    contract: "Full Time",
+    location: "USA only",
+    tags: {
+      new: true,
+      featured: true
+    },
+    filters: {
+      role: "Frontend",
+      level: "Senior",
+      techs: ["HTML", "CSS", "JavaScript"]
+    }
+  },
+  {
+    company: "Manage",
+    jobTitle: "Fullstack Developer",
+    date: "1d ago",
+    contract: "Part Time",
+    location: "Remote",
+    tags: {
+      new: true,
+      featured: true
+    },
+    filters: {
+      role: "Fullstack",
+      level: "Midweight",
+      techs: ["Python", "React"]
+    }
+  },
+  {
+    company: "Account",
+    jobTitle: "Junior Frontend Developer",
+    date: "2d ago",
+    contract: "Part Time",
+    location: "USA only",
+    tags: {
+      new: true,
+      featured: false
+    },
+    filters: {
+      role: "Frontend",
+      level: "Junior",
+      techs: ["React", "Sass", "JavaScript"]
+    }
+  },
+  {
+    company: "MyHome",
+    jobTitle: "Junior Frontend Developer",
+    date: "5d ago",
+    contract: "Contract",
+    location: "USA only",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Frontend",
+      level: "Junior",
+      techs: ["CSS", "JavaScript"]
+    }
+  },
+  {
+    company: "Loop Studios",
+    jobTitle: "Software Engineer",
+    date: "1w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Fullstack",
+      level: "Midweight",
+      techs: ["JavaScript", "Sass", "Ruby"]
+    }
+  },
+  {
+    company: "FaceIt",
+    jobTitle: "Junior Backend Developer",
+    date: "2w ago",
+    contract: "Full Time",
+    location: "UK only",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Backend",
+      level: "Junior",
+      techs: ["Ruby", "RoR"]
+    }
+  },
+  {
+    company: "Shortly",
+    jobTitle: "Junior Developer",
+    date: "2w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Frontend",
+      level: "Junior",
+      techs: ["HTML", "Sass", "JavaScript"]
+    }
+  },
+  {
+    company: "Insure",
+    jobTitle: "Junior Frontend Developer",
+    date: "2w ago",
+    contract: "Full Time",
+    location: "USA only",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Frontend",
+      level: "Junior",
+      techs: ["Vue", "JavaScript", "Sass"]
+    }
+  },
+  {
+    company: "Eyecam Co.",
+    jobTitle: "Full Stack Engineer",
+    date: "3w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Fullstack",
+      level: "Midweight",
+      techs: ["JavaScript", "Django", "Python"]
+    }
+  },
+  {
+    company: "The Air Filter Company",
+    jobTitle: "Front-end Dev",
+    date: "1mo ago",
+    contract: "Part Time",
+    location: "Worldwide",
+    tags: {
+      new: false,
+      featured: false
+    },
+    filters: {
+      role: "Frontend",
+      level: "Junior",
+      techs: ["React", "Sass", "JavaScript"]
+    }
+  }
+]
+
